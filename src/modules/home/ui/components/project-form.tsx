caabcda31b32eef7f62025b1fbf321f6e1eae531
Atul Kumar Singh <65683432+aksxy007@users.noreply.github.com>
2025-07-12 15:11:58 +0530

    17: Billing

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -32,16 +32,20 @@ export const ProjectForm = () => {
         queryClient.invalidateQueries(
             trpc.projects.getMany.queryOptions()
         )
+        queryClient.invalidateQueries(
+            trpc.usage.status.queryOptions()
+        )
 
         router.push(`/projects/${data.id}`)
-        // TODO Invalidate usage status
     },
     onError: (error)=>{
-        // TODO: Redirect to pricing page
         toast.error(error.message)
         if (error.data?.code === 'UNAUTHORIZED'){
           // clerk.openSignIn();
           router.push(`sign-in`)
+        }
+        if (error.data?.code == 'TOO_MANY_REQUESTS'){
+          router.push("/pricing")
         }
     }
   }));
